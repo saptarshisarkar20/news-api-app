0b7d5e6a546ed3c5c19cb5149fe51a88f582e17b
Saptarshi Sarkar <saptarshi2017cob@gmail.com>
2021-12-12 23:12:12 +0530

    added api of sayak

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Static Files
 app.use(express.static("public"));
